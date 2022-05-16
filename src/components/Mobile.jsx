6565e1e720ceb69abbe4cff81481a59733fb824e
Imran Khan <99397606+imran120198@users.noreply.github.com>
2022-05-16 21:47:36 +0530

    first commit

---
+++ src/components/Mobile.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Mobile = () => {
+    return <>
+        <h1>Mobile Operating System</h1>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+
+        <h1>Mobile Manufactures</h1>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+    </>
+}
+
+export default Mobile;
